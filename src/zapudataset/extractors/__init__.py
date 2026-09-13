3a641e6aee08431719eebbc4449265694dc6b1f6
masterofray <aryanto.dandan@gmail.com>
2026-09-13 12:53:22 +0900

    chore(extractors): initialize extractor module exports

--- src/zapudataset/extractors/__init__.py
+++ src/zapudataset/extractors/__init__.py
@@ -14,11 +14,13 @@ from .kaggle_loader        import load_kaggle_behavior, load_kaggle_orders
 from .ga4_stream_simulator import simulate_ga4_events
 from .bigquery_connect     import BigQueryExporter
 from .kaggle_setup         import KaggleSetup, KaggleDown
+from .bqinits              import BQcred
 
 __all__ = ['load_kaggle_behavior',
            'load_kaggle_orders',
            'simulate_ga4_events',
            'BigQueryExporter',
            'KaggleSetup', 
-           'KaggleDown',]
+           'KaggleDown',
+           'BQcred',]
 

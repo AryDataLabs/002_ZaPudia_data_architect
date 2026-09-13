2e42830c095c01417aa8ce758baf7fce55fe624b
masterofray <aryanto.dandan@gmail.com>
2026-09-13 12:53:24 +0900

    chore(noise): initialize synthetic noise module package

--- src/zapudataset/noise/__init__.py
+++ src/zapudataset/noise/__init__.py
@@ -1,1 +1,28 @@
-from noise.anomaly_injector import NoiseConfig, inject_all
+#!/usr/bin/env python3
+
+__author__     = "Aryanto"
+__copyright__  = "Copyright 2026, AryDataLabs/ZaPuDia Series"
+__credits__    = ["aryanto"]
+__license__    = "GNU_Public"
+__version__    = "0.0.1"
+__maintainer__ = "Aryanto"
+__email__      = "aryanto.dandan@gmail.com"
+__created__    = "2026-08-31"
+__modified__   = "2026-09-13"
+
+from .anomaly_injector import (NoiseConfig, 
+                               inject_schema_degradation, 
+                               partition_cold_items, 
+                               inject_out_of_order, 
+                               inject_telemetry_drops, 
+                               inject_bot_flood, 
+                               inject_all,)
+
+__all__ = ['NoiseConfig',
+           'inject_schema_degradation',
+           'partition_cold_items',
+           'inject_out_of_order',
+           'inject_telemetry_drops',
+           'inject_bot_flood',
+           'inject_all',]
+

5cb832178193be54bb5b3f8325d4db5fb7c53792
masterofray <aryanto.dandan@gmail.com>
2026-09-13 14:21:55 +0900

    add pipeline package init file to handle module exports

--- src/zapudataset/pipeline/__init__.py
+++ src/zapudataset/pipeline/__init__.py
@@ -1,1 +1,22 @@
-pass
+#!/usr/bin/env python3
+
+__author__     = "Aryanto"
+__copyright__  = "Copyright 2026, AryDataLabs/ZaPuDia Series"
+__credits__    = ["aryanto"]
+__license__    = "GNU_Public"
+__version__    = "0.0.1"
+__maintainer__ = "Aryanto, M.Si"
+__email__      = "aryanto.dandan@gmail.com"
+__created__    = "2026-08-31"
+__modified__   = "2026-09-13"
+
+
+from .internal_config import PipelineConfig
+from .orchestrator    import DatasetPipeline, Orchestrator
+from .stages          import FeatureEngineeringStage, ImplicitMatrixStage
+
+__all__ = ['PipelineConfig',
+           'DatasetPipeline',
+           'Orchestrator',
+           'FeatureEngineeringStage',
+           'ImplicitMatrixStage',]
